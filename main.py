112b6665eff44ee21258fdd3cfe7310f05f65bf9
Chetan badgujar <chetan.badgujar@cyberium.info>
2023-05-29 16:59:23 +0530

    grap_ generation_code

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from sentiment_a import *
 from train import model
 from scrap import ScrapComment
 
+import matplotlib.pyplot as plt
+
 ########### enter url ########
-url="https://www.youtube.com/watch?v=mBcBoGhFndY"
-df =   ScrapComment(url)
-#df.to_csv('comment.csv')
+# url="https://www.youtube.com/watch?v=mBcBoGhFndY"
+# df =   ScrapComment(url)
+# f.to_csv('comment.csv')d
 print('***********data scrapping completed***********')
 
 ###for csv file##############
-#file_path='comment.csv'
-#df=datatranformation(file_path)
+file_path='comment.csv'
+df=datatranformation(file_path)
 
 #############################
 labeldata =  labeling(df)
@@ -22,6 +24,18 @@ Xdata,Ydata = sampling(proc_data)
 cm,accuracy= model(Xdata,Ydata)
 print(cm)
 #print(accuracy*100, '%')
-print('Accuracy:',accuracy*100,'%')
+#print('Accuracy:',accuracy*100,'%')
+positive_score= cm[0][0]
+neutral_score=cm[1][1]
+negative_score=cm[2][2]
+
+left = [1, 2, 3]
+height = [positive_score,neutral_score,negative_score]
+tick_label = ['Positive', 'Neutral', 'Negative']
+plt.bar(left, height, tick_label = tick_label,
+        width = 0.8, color = [ 'green','yellow','red'])
+plt.ylabel('Scores (percentage)')
+plt.title('Video Comments Analysis')
+plt.show()
 
 print('done')
--- yapi.py
+++ yapi.py
@@ -3,24 +3,51 @@ import requests
     
 from fastapi.responses import FileResponse
 
+#app = FastAPI()
+
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Welcome to our channel. please like, share and subscribe"}
+
+
+# @app.post("/process_youtube_link")
+# async def process_youtube_link(link: str):
+#     return {"message": "Link processed successfully"}
+
+
+
+
+# file_path = "F:\Project\youtube comment sentiment analysis\dog_image.jpg"
+
+
+# @app.get("/image")
+# async def image():
+#     return FileResponse(file_path)
+
+#####
+import requests
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Welcome to our channel. please like, share and subscribe"}
+BASE_URL = "http://localhost:8000"
 
 
-@app.post("/process_youtube_link")
-async def process_youtube_link(link: str):
-    return {"message": "Link processed successfully"}
+def process_url(url: str):
+    endpoint = BASE_URL + "/process_url"
+    data = {"url": url}
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(endpoint, json=data, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"message": "Failed to process URL"}
 
 
+def main():
+    url = input("Enter URL: ")
+    result = process_url(url)
+    print(result)
 
 
-file_path = "F:\Project\youtube comment sentiment analysis\dog_image.jpg"
-
-
-@app.get("/image")
-async def image():
-    return FileResponse(file_path)
+if __name__ == "__main__":
+    main()
